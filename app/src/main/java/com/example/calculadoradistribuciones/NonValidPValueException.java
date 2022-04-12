4d588f51262d901d0ad76513452897f8a6740f90
syarzabal <90835403+syarzabal@users.noreply.github.com>
2022-04-12 11:17:40 +0200

    Áñadidas excepciones de valor no valido de p y de campos vacíos

---
+++ app/src/main/java/com/example/calculadoradistribuciones/NonValidPValueException.java
@@ -0,0 +1,7 @@
+package com.example.calculadoradistribuciones;
+
+public class NonValidPValueException extends Exception{
+    public NonValidPValueException(String message){
+        super(message);
+    }
+}
